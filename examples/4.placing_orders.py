7e62e8520e7761958499d0e00237a149fcc7209f
ibadia <ibad2762@gmail.com>
2023-08-11 16:36:35 +0500

    first commit copy paste code

---
+++ examples/4.placing_orders.py
@@ -0,0 +1,86 @@
+from config import TEST_ACCT_KEY, TEST_NETWORK
+from firefly_exchange_client import FireflyClient, Networks, MARKET_SYMBOLS, ORDER_SIDE, ORDER_TYPE, OrderSignatureRequest
+import asyncio
+
+
+
+async def place_limit_order(client: FireflyClient):
+   
+    # default leverage of account is set to 3 on firefly
+    user_leverage = await client.get_user_leverage(MARKET_SYMBOLS.ETH)
+
+    # creates a LIMIT order to be signed
+    signature_request = OrderSignatureRequest(
+        symbol=MARKET_SYMBOLS.ETH,  # market symbol
+        price=1632.8,  # price at which you want to place order
+        quantity=0.01, # quantity
+        side=ORDER_SIDE.BUY, 
+        orderType=ORDER_TYPE.LIMIT,
+        leverage=user_leverage
+    )  
+
+    # create signed order
+    signed_order = client.create_signed_order(signature_request) 
+
+    print("Placing a limit order")
+    # place signed order on orderbook
+    resp = await client.post_signed_order(signed_order)
+
+    # returned order with PENDING state
+    print(resp)
+
+    return
+
+async def place_market_order(client: FireflyClient):
+    
+
+    # default leverage of account is set to 3 on firefly
+    user_leverage = await client.get_user_leverage(MARKET_SYMBOLS.ETH)
+
+    # creates a LIMIT order to be signed
+    signature_request = OrderSignatureRequest(
+        symbol=MARKET_SYMBOLS.ETH,  # market symbol
+        price=0,  # price at which you want to place order
+        quantity=0.01, # quantity
+        side=ORDER_SIDE.BUY, 
+        orderType=ORDER_TYPE.MARKET,
+        leverage=user_leverage
+    )  
+
+    # create signed order
+    signed_order = client.create_signed_order(signature_request) 
+
+    print("Placing a market order")
+    # place signed order on orderbook
+    resp = await client.post_signed_order(signed_order)
+
+    # returned order with PENDING state
+    print(resp)
+
+
+    return
+
+async def main():
+
+    # initialize client
+    client = FireflyClient(
+        True, # agree to terms and conditions
+        Networks[TEST_NETWORK], # network to connect with
+        TEST_ACCT_KEY, # private key of wallet
+        )
+
+    await client.init(True) 
+
+    # add market that you wish to trade on ETH/BTC are supported currently
+    client.add_market(MARKET_SYMBOLS.ETH)
+
+    # await place_limit_order(client)
+    await place_market_order(client)
+    
+    await client.close_connections()
+
+
+if __name__ == "__main__":
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(main())
+  loop.close()
---
+++ examples/7.cancelling_orders.py
@@ -0,0 +1,62 @@
+import time
+from config import TEST_ACCT_KEY, TEST_NETWORK
+from firefly_exchange_client import FireflyClient, Networks, MARKET_SYMBOLS, ORDER_SIDE, ORDER_TYPE, ORDER_STATUS
+from pprint import pprint
+import asyncio
+
+
+async def main():
+
+    client = FireflyClient(True, Networks[TEST_NETWORK], TEST_ACCT_KEY)
+    await client.init(True)
+
+    # must add market before cancelling its orders
+    client.add_market(MARKET_SYMBOLS.ETH)
+
+    order = {
+        "symbol":MARKET_SYMBOLS.ETH, 
+        "price":1301, 
+        "quantity":0.1, 
+        "side":ORDER_SIDE.SELL, 
+        "orderType":ORDER_TYPE.LIMIT,
+        "leverage":await client.get_user_leverage(MARKET_SYMBOLS.ETH),
+        "expiration":int(time.time()+(30*24*60*60)), # a random time in future
+        "reduceOnly":False,
+        "salt":10,
+        "postOnly":True
+    }
+
+
+    signed_order = client.create_signed_order(order) 
+    resp = await client.post_signed_order(signed_order)
+    
+    
+    # sign order for cancellation using order hash
+    # you can pass a list of hashes to be signed for cancellation, good to be used when multiple orders are to be cancelled
+    cancellation_request = client.create_signed_cancel_orders(MARKET_SYMBOLS.ETH, order_hash=[resp['hash']])
+    pprint(cancellation_request)
+
+    # # or sign the order for cancellation using order data
+    cancellation_request = client.create_signed_cancel_order(order)
+    pprint(cancellation_request) # same as above cancellation request
+
+    # post order to exchange for cancellation
+    resp = await client.post_cancel_order(cancellation_request)
+    
+    pprint(resp)
+
+    # cancels all open orders, returns false if there is no open order to cancel
+    resp = await client.cancel_all_orders(MARKET_SYMBOLS.ETH, [ORDER_STATUS.OPEN, ORDER_STATUS.PARTIAL_FILLED])
+
+    if resp == False:
+        print('No open order to cancel')
+    else:
+        pprint(resp)
+
+    await client.close_connections()
+
+
+if __name__ == "__main__":
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(main())
+  loop.close()
